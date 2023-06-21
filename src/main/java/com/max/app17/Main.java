b2091d45d1854fd5356e1a02a877420ac688d482
mstepan <maksym.stepanenko@oracle.com>
2023-06-21 10:52:16 +0300

    Small changes

--- src/main/java/com/max/app17/Main.java
+++ src/main/java/com/max/app17/Main.java
@@ -6,5 +6,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Main done...");
     }
+    /*
+    /Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home/bin/jmod  describe /Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home/jmods/java.base.jmod
+
+     jmod  describe /Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home/jmods/java.base.jmod
+     */
 
 }

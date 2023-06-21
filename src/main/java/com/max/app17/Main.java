15a2aecc83561d5dda52ac2fdc47498535143fe0
mstepan <maksym.stepanenko@oracle.com>
2023-06-21 14:41:53 +0300

    Few mediaum level leetcode tasks added.

--- src/main/java/com/max/app17/Main.java
+++ src/main/java/com/max/app17/Main.java
@@ -4,12 +4,7 @@ package com.max.app17;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+
         System.out.println("Main done...");
     }
-    /*
-    /Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home/bin/jmod  describe /Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home/jmods/java.base.jmod
-
-     jmod  describe /Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home/jmods/java.base.jmod
-     */
-
 }

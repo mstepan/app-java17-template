47068f9e02a282fd826f5a0c09c5e4235f22eae7
mstepan <maksym.stepanenko@oracle.com>
2023-07-20 10:23:56 +0300

    2SUMCloses leetcode solutin added

--- src/main/java/com/max/app17/Main.java
+++ src/main/java/com/max/app17/Main.java
@@ -1,50 +1,11 @@
 package com.max.app17;
 
-import java.math.BigInteger;
-import java.util.stream.Stream;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        LargeFibonacciProvider fibProvider = new LargeFibonacciProvider();
-
-        Stream.generate(fibProvider::next).
-                limit(200_000).
-                forEach(System.out::println);
-
         System.out.println("Main done...");
     }
-
-    private static class LargeFibonacciProvider {
-        BigInteger first = BigInteger.ZERO;
-        BigInteger second = BigInteger.ONE;
-
-        public BigInteger next(){
-            BigInteger cur = first;
-
-            first = second;
-
-            second = second.add(cur);
-            return cur;
-        }
-    }
-
-    private static class IntFibonacciProvider {
-        int first = 0;
-        int second = 1;
-
-        public int next() {
-            int cur = first;
-
-            if( cur < 0 ){
-                throw new IllegalStateException("Fibonacci int overflow detected");
-            }
-
-            first = second;
-            second += cur;
-            return cur;
-        }
-    }
-
 }

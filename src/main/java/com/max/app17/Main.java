aee673377ef68f2ef99297e8727287501acea5d4
mstepan <maksym.stepanenko@oracle.com>
2023-08-14 12:15:15 +0300

    352. Data Stream as Disjoint Intervals solved

--- src/main/java/com/max/app17/Main.java
+++ src/main/java/com/max/app17/Main.java
@@ -4,7 +4,11 @@ package com.max.app17;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        
 
         System.out.println("Main done...");
     }
+    
+
+
 }

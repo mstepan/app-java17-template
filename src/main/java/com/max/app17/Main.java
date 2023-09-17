60cb4cf7a4c885df2e65b19997f32a4ef0b0a400
mstepan <maksym.stepanenko@oracle.com>
2023-09-17 11:33:06 +0300

    Leetcode solved. 239. Sliding Window Maximum

--- src/main/java/com/max/app17/Main.java
+++ src/main/java/com/max/app17/Main.java
@@ -1,9 +1,8 @@
 package com.max.app17;
 
-
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         System.out.println("Main done...");
     }

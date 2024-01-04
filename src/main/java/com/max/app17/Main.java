4e7e5b36f5b055cb0a7a38b24b89b281f62358b9
mstepan <maksym.stepanenko@oracle.com>
2024-01-04 09:53:22 +0200

    633. Sum of Square Numbers

--- src/main/java/com/max/app17/Main.java
+++ src/main/java/com/max/app17/Main.java
@@ -1,13 +1,10 @@
 package com.max.app17;
 
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        
 
         System.out.println("Main done...");
     }
-
 
 }

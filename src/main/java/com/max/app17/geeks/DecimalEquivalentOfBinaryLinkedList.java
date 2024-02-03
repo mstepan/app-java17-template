55f0029e4db32b8cadda965868e6bec33c227578
mstepan <maksym.stepanenko@oracle.com>
2024-02-03 16:50:56 +0200

    Few geeks tasks solved

---
+++ src/main/java/com/max/app17/geeks/DecimalEquivalentOfBinaryLinkedList.java
@@ -0,0 +1,59 @@
+package com.max.app17.geeks;
+
+/*
+ * Decimal Equivalent of Binary Linked List
+ * https://www.geeksforgeeks.org/problems/decimal-equivalent-of-binary-linked-list/1?utm_source=geeksforgeeks&utm_medium=newui_home&utm_campaign=potd
+ *
+ */
+public class DecimalEquivalentOfBinaryLinkedList {
+
+    public static void main(String[] args) throws Exception {
+
+        LinkedList root = new LinkedList();
+
+        // {1, 1, 1, 0}
+        root.head = new Node(1, new Node(1, new Node(1, new Node(0))));
+
+        long res = DecimalValue(root.head);
+
+        System.out.printf("res = %d%n", res);
+
+        System.out.println("DecimalEquivalentOfBinaryLinkedList done...");
+    }
+
+    private static final int MOD = 1_000_000_007;
+
+    static long DecimalValue(Node head) {
+        Node cur = head;
+
+        int res = 0;
+
+        while (cur != null) {
+            int digit = cur.data;
+            res = (res * 2 + digit) % MOD;
+            cur = cur.next;
+        }
+
+        return res;
+    }
+
+    // === DO NOT COPY below code ====
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        Node(int data) {
+            this(data, null);
+        }
+    }
+
+    static class LinkedList {
+        Node head;
+    }
+}
---
+++ src/main/java/com/max/app17/geeks/ImplementAtoi.java
@@ -0,0 +1,59 @@
+package com.max.app17.geeks;
+
+public class ImplementAtoi {
+
+    public static void main(String[] args) throws Exception {
+
+        String str = "-123";
+
+        System.out.println(atoi(str));
+
+        System.out.println("ImplementAtoi done...");
+    }
+
+    /**
+     *
+     * Implement Atoi
+     *
+     * https://www.geeksforgeeks.org/problems/implement-atoi/1?utm_source=geeksforgeeks&utm_medium=newui_home&utm_campaign=potd
+     *
+     * Below function may overflow when str = "9_999_999_999" but that's expected
+     *
+     * Constraints:
+     * 1 ≤ |s| ≤ 10
+     */
+    static int atoi(String initialStr) {
+        if (initialStr == null) {
+            throw new IllegalArgumentException("'initialStr' is null");
+        }
+
+        String str = initialStr.trim();
+
+        if (str.isEmpty()) {
+            return -1;
+        }
+
+        if ("-".equals(str)) {
+            return -1;
+        }
+
+        final int sign = (str.charAt(0) == '-') ? -1 : 1;
+
+        int val = 0;
+
+        final int startIdx = (sign == -1) ? 1 : 0;
+
+        for (int i = startIdx; i < str.length(); ++i) {
+            char ch = str.charAt(i);
+
+            if (ch < '0' || ch > '9') {
+                return -1;
+            }
+
+            val = val * 10 + (ch - '0');
+        }
+
+        return sign * val;
+    }
+
+}

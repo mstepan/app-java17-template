e110bb5efa4d19a0aaf8dc74f0e9498b4702dbcb
mstepan <maksym.stepanenko@oracle.com>
2024-02-11 14:55:45 +0200

    Minor chnages

--- src/main/java/com/max/app17/Main.java
+++ src/main/java/com/max/app17/Main.java
@@ -3,6 +3,10 @@ package com.max.app17;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+    
+        for(int i = 0; i < 10; ++i){
+          System.out.println(i);
+        }
 
         System.out.println("Main done...");
     }

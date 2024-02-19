97d390d4c56de12bafd24a3b59209b2d74b1a576
mstepan <maksym.stepanenko@oracle.com>
2024-02-19 10:09:36 +0200

    Eclipse files added to ignore b/c neovim jdt creates few

--- src/main/java/com/max/app17/Main.java
+++ src/main/java/com/max/app17/Main.java
@@ -2,8 +2,14 @@ package com.max.app17;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
 
-        System.out.println("Main done...");
+    int x = 133;
+
+    if (x > 10) {
+      System.out.println(x);
     }
+
+    System.out.println("Main done...");
+  }
 }

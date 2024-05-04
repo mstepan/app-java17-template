9e7df0a1db5f34533bb951b082adecc3036051da
mstepan <maksym.stepanenko@oracle.com>
2024-05-04 12:32:27 +0300

    Leetcode solved. 316. Remove Duplicate Letters

--- src/main/java/com/github/mstepan/app17/Main.java
+++ src/main/java/com/github/mstepan/app17/Main.java
@@ -1,8 +1,24 @@
 package com.github.mstepan.app17;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
+
+        System.out.println("Counter started");
+
+        AtomicLong val = new AtomicLong(0L);
+        while (!Thread.currentThread().isInterrupted()) {
+            val.incrementAndGet();
+
+            TimeUnit.SECONDS.sleep(60L);
+
+            System.out.printf("counter: %d%n", val.get());
+        }
+
+        System.out.printf("Final counter: %d%n", val.get());
 
         System.out.println("Main done...");
     }

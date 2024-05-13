afe9d3b4c2eb70a5aec99c35333003ac67d85624
mstepan <maksym.stepanenko@oracle.com>
2024-05-13 19:29:03 +0300

    Leetcode solved. 40. Combination Sum II

--- src/main/java/com/github/mstepan/app17/Main.java
+++ src/main/java/com/github/mstepan/app17/Main.java
@@ -1,18 +1,9 @@
 package com.github.mstepan.app17;
 
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        char[] arr = "catsanddog".toCharArray();
-
-        int left = 0;
-        int right = 2;
-
-        String sub = new String(arr, left, right - left + 1);
-
-        System.out.println(sub);
 
         System.out.println("Main done...");
     }
--- src/main/java/com/github/mstepan/app17/leetcode/hard/WordBreak2.java
+++ src/main/java/com/github/mstepan/app17/leetcode/hard/WordBreak2.java
@@ -2,7 +2,6 @@ package com.github.mstepan.app17.leetcode.hard;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;

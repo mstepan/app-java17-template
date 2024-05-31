6fe1cdaf907223f1c298d1c382d6bffd47b2377e
mstepan <maksym.stepanenko@oracle.com>
2024-05-31 10:56:30 +0300

    Leetcode solved. 3159. Find Occurrences of an Element in an Array

--- src/main/java/com/github/mstepan/app17/Main.java
+++ src/main/java/com/github/mstepan/app17/Main.java
@@ -1,8 +1,12 @@
 package com.github.mstepan.app17;
 
+import java.util.UUID;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
+
+        System.out.println(UUID.randomUUID().toString());
 
         System.out.println("Main done...");
     }

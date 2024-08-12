e88bb9aad5787912dc4298e66705289c8684e1fc
mstepan <maksym.stepanenko@oracle.com>
2024-08-12 10:57:32 +0300

    Integration-like testing added to timer wheels

--- src/main/java/com/github/mstepan/app17/timer/TimerMain.java
+++ src/main/java/com/github/mstepan/app17/timer/TimerMain.java
@@ -4,30 +4,38 @@ import java.time.Instant;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class TimerMain {
 
     public static void main(String[] args) throws Exception {
-        HashedHierarchicalTimingWheels timeWheels = HashedHierarchicalTimingWheels.newInstance();
+        final HashedHierarchicalTimingWheels timeWheels = HashedHierarchicalTimingWheels.newInstance();
 
-        Instant now = Instant.now();
+        final Instant now = Instant.now();
 
-        final int threadsCount = 5;
+        final int threadsCount = 200;
+        final int callbacksCountPerThread = 10_000;
+        final int maxPossibleDelayInSec = 30;
+
         final CountDownLatch allCompleted = new CountDownLatch(threadsCount);
 
         ExecutorService pool = Executors.newFixedThreadPool(threadsCount);
+
+        final AtomicInteger counter = new AtomicInteger(0);
 
         for (int th = 0; th < threadsCount; ++th) {
             pool.execute(
                     () -> {
                         try {
-                            for (int delay = 5; delay < 100; delay += 5) {
-                                final int delayInSec = delay;
+                            ThreadLocalRandom rand = ThreadLocalRandom.current();
+
+                            for (int it = 0; it < callbacksCountPerThread; ++it) {
+                                int delayInSec = 1 + rand.nextInt(maxPossibleDelayInSec);
 
                                 timeWheels.addCallback(
-                                        now.plusSeconds(delayInSec),
-                                        () -> System.out.printf("%d seconds%n", delayInSec));
+                                        now.plusSeconds(delayInSec), counter::incrementAndGet);
                             }
                         } finally {
                             allCompleted.countDown();
@@ -41,7 +49,15 @@ public class TimerMain {
 
         pool.shutdownNow();
 
-        TimeUnit.MINUTES.sleep(1);
+        TimeUnit.SECONDS.sleep(maxPossibleDelayInSec + 5);
+
+        if (counter.get() != threadsCount * callbacksCountPerThread) {
+            throw new AssertionError(
+                    "Counter value is incorrect, expected = %d, actual = %d"
+                            .formatted(threadsCount * callbacksCountPerThread, counter.get()));
+        }
+
+        System.out.printf("counter: %d%n", counter.get());
 
         System.out.println("TimerMain done...");
     }
